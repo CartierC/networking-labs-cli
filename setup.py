3d05739d60d7474dbe32e34989be1f143ea3cb79
Colossal Cartier <165856560+CartierC@users.noreply.github.com>
2026-04-16 12:53:09 -0400

    Add author information and URL to setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
-from setuptools import setup, find_packages
-
 setup(
     name="networking-labs-cli",
     version="1.0.0",
+    author="Carter",
+    author_email="justn.carter9527@yahoo.com",
+    url="https://github.com/CartierC/networking-labs-cli",
     packages=find_packages(),
     install_requires=["boto3", "botocore"],
     entry_points={
